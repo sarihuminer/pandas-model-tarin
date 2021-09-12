b60eecec3ca96d34fce10b245e6f09c4dc87b466
sari shteren <sh6197174@gmail.com>
2021-09-12 03:46:05 +0300

    train model succsesfull -score 1.0

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 "# pandas-model-tarin"
+
 to get the env -you have requirements.txt file
+##The Files:
+create model- is the file of ml -to detect if ref is good or not.
+
+controller file-get from the angular the path of file.
+
+pandasTry- create confussion matrix from static arrays.
+
+
--- create model.py
+++ create model.py
@@ -2,6 +2,33 @@ import pandas as pd
 import numpy as np
 
 import seaborn as sns
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, LabelEncoder
+
+
+def encode_and_bind(original_dataframe, index_feature_to_encode):
+    # pd.get_dummies(X_train[2])
+    dummies = pd.get_dummies(original_dataframe[index_feature_to_encode])
+    res = pd.concat([original_dataframe, dummies], axis=1)
+    return (res)
+
+
+# prepare input data
+def prepare_inputs(X_train, X_test):
+    oe = OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=np.nan)
+    oe.fit(X_train)
+    X_train_enc = oe.transform(X_train)
+    X_test_enc = oe.transform(X_test)
+    return X_train_enc, X_test_enc
+
+
+# prepare target
+def prepare_targets(y_train, y_test):
+    le = LabelEncoder()
+    le.fit(y_train)
+    y_train_enc = le.transform(y_train)
+    y_test_enc = le.transform(y_test)
+    return y_train_enc, y_test_enc
+
 
 excel_file = 'WikiRef-input.xlsx'
 # data = pd.read_excel(excel_file)
@@ -14,8 +41,11 @@ sns.pairplot(dataset.iloc[:, 0:11], hue='isRefQK')
 # הפרדה על מנת לאמן את הנתונים
 x = dataset.iloc[:, 0:11].values
 y = dataset.iloc[:, 8].values
-
-#קבלת הסוגים
+# format all fields as string
+X = x.astype(str)
+# reshape target to be a 2d array
+y = y.reshape((len(y), 1))
+# קבלת הסוגים
 
 species_names = np.unique(np.array(y[np.logical_not(np.isnan(y))]))
 
@@ -29,24 +59,46 @@ print(y_cat.sample(5))
 X = pd.DataFrame(x)
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X.values, y_cat, test_size=0.25,random_state=42)
-#X_train, X_test, y_train, y_test = train_test_split(X.values, y, test_size=0.25, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X.values, y_cat, test_size=0.25, random_state=42)
+# X_train, X_test, y_train, y_test = train_test_split(X.values, y, test_size=0.25, random_state=42)
 # אימון המודל
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+from sklearn.feature_extraction import DictVectorizer
+
+vec = DictVectorizer()
+# prepare input data
+X_train_enc, X_test_enc = prepare_inputs(X_train, X_test)
+# prepare output data
+y_train_enc, y_test_enc = prepare_targets(y_train, y_test)
+
+clf = RandomForestClassifier()
+clf.fit(X_train_enc, y_train_enc)
+print(clf.predict(np.nan_to_num(X_test_enc)))
+print(y_test)
+print(clf.score(np.nan_to_num(X_test_enc), y_test_enc))
+#clf.score(np.nan_to_num(X_test_enc), y_test_enc)
+#### you need tensorflow 2.2 or 3.3 to run it###
 import keras
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import adam_v2
+#from keras.models import Sequential
+#from keras.layers import Dense
+#from keras.optimizers import adam_v2
 
-model = Sequential()
-model.add(Dense(2, input_shape=(9,), activation='softmax'))
-model.compile(adam_v2.Adam(lr=0.1), loss='categorical_crossentropy', metrics=['accuracy'])
+#model = Sequential()
+#model.add(Dense(2, input_shape=(9,), activation='softmax'))
+#model.compile(adam_v2.Adam(lr=0.1), loss='categorical_crossentropy', metrics=['accuracy'])
 
-from keras.callbacks import EarlyStopping
+#from keras.callbacks import EarlyStopping
 
-es = EarlyStopping(monitor='val_loss', min_delta=0.001, patience=5, verbose=1, mode='auto')
+#es = EarlyStopping(monitor='val_loss', min_delta=0.001, patience=5, verbose=1, mode='auto')
 
-model.fit(X_train, y_train, validation_data=(X_test, y_test), callbacks=[es], epochs=500)
-y_pred = model.predict(X_test)
-print(y_pred[:5])
-y_pred_class = np.argmax(y_pred, axis=1)
-print(y_pred_class)
+#model.fit(X_train, y_train, validation_data=(X_test, y_test), callbacks=[es], epochs=500)
+#y_pred = model.predict(X_test)
+#print(y_pred[:5])
+#y_pred_class = np.argmax(y_pred, axis=1)
+#print(y_pred_class)
+
+###end tnsoflow 2.2 -3.3 v##
